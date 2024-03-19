3dc37361f3c2b173e437d45a8e5af06f69091271
NjinoNjuguna <50864034+NjinoNjuguna@users.noreply.github.com>
2024-03-19 04:35:45 +0300

    Create currency.py

---
+++ itry.py/currency.py
@@ -0,0 +1,8 @@
+currency_rates = {}
+currency_rates['EUR']=0.85
+currency_rates['GBP']=0.72
+currency_rates['JPY']=109.35
+currency_rates['CAD']=1.21
+gbp_rate=currency_rates['GBP']
+print(gbp_rate)
+print(currency_rates)
